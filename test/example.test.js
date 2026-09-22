75120a006ddfe42834ad6cb2b03bc5adf6ce9ce8
munna-pp <153918999+munna-pp@users.noreply.github.com>
2026-09-22 16:20:13 +0530

    ggg

--- test/example.test.js
+++ test/example.test.js
@@ -2,5 +2,5 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 
 test('basic calculation works', () => {
-  assert.equal(1 + 1, 2);
+  assert.equal(1 + 1, 3);
 });
